5be34e6c8f087e23246c73055c7fe85e2d18bebc
Kai Poon <k4poon@ucsd.edu>
2022-05-24 13:23:55 -0700

    Update speech_rec.py
    - added sound engine
    - added ambient noise filter

--- speech_rec.py
+++ speech_rec.py
@@ -1,7 +1,7 @@
 import speech_recognition as sr
 import time
 import threading
-
+import pyttsx3
 flag = 0
 
 
@@ -13,23 +13,53 @@ def terminate():
 
 
 speech_rec_obj = sr.Recognizer()
+count = 0
+engine = pyttsx3.init()
+
 while True:
     with sr.Microphone() as source:
-        print("Speak anything ")
-        terminate_thread = threading.Thread(target=terminate)
-        terminate_thread.start()
 
-        speech_rec_obj.adjust_for_ambient_noise(source, duration=0.2)
-        audio = speech_rec_obj.listen(source, phrase_time_limit=1)
+        # reduce ambient noise
+        if count == 0:
+            engine.say("Please be quiet, calibration starts in 3 seconds")
+            engine.runAndWait()
+
+            # time.sleep(0.1)
+            engine.say("3")
+            engine.runAndWait()
+
+            # time.sleep(0.1)
+            engine.say("2")
+            engine.runAndWait()
+
+            # time.sleep(0.1)
+            engine.say("1")
+            engine.runAndWait()
+
+            speech_rec_obj.adjust_for_ambient_noise(source, duration=1)
+            engine.say("Calibration done.")
+            engine.runAndWait()
+            count = 1
+
+        engine.say("Speak Anything")
+        engine.runAndWait()
+        print("speak")
+        # terminate_thread = threading.Thread(target=terminate)
+        # terminate_thread.start()
+        audio = speech_rec_obj.listen(source, phrase_time_limit=1.5)
 
         try:
             text = speech_rec_obj.recognize_google(audio)
-            print(f"You said: {text}")
+            engine.say(f"You said: {text}")
+            engine.runAndWait()
 
         except:
-            print("Sorry could not recognize your voice.")
+            engine.say("Sorry could not recognize your voice.")
+            engine.runAndWait()
+            text = " "
 
-        if flag == 1:
-            print("End speech recognition.")
+        if text == "end" or text == "end program" or text == "exit" or text == "exit program":
+            engine.say("Exiting program")
+            engine.runAndWait()
             break
-        time.sleep(0.5)
+        # time.sleep(0.5)
